e8f3121b5622ada19d9d317384a3d27e54b6a6c0
Jeremy Moore <damoores@gmail.com>
2016-08-02 09:37:34 -0700

    add carthage

---
+++ StackOverflow/CustomError.m
@@ -0,0 +1,15 @@
+//
+//  CustomError.m
+//  StackOverflow
+//
+//  Created by Jeremy Moore on 8/1/16.
+//  Copyright © 2016 Jeremy Moore. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomError : NSObject
+
+
+
+@end
